7f44459375a6f932ccc4e75d929152a817e981ea
Kaio Cesar <programador.kaio@gmail.com>
2014-09-21 14:28:15 -0300

    Front-end

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 import os
 from werkzeug import secure_filename
-from flask import Flask, request, render_template, redirect, url_for, current_app
+from flask import Flask, request, render_template, redirect, url_for, current_app, send_from_directory
 from db import Articles
 
 
@@ -11,9 +11,14 @@ app = Flask(__name__, static_url_path='')
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 app.config['MEDIA_ROOT'] = os.path.join(PROJECT_ROOT, 'media_files')
 
+@app.route("/media/<path:filename>")
+def media(filename):
+	return send_from_directory(current_app.config.get("MEDIA_ROOT"), filename)
+
 @app.route("/")
 def index():
-	return render_template("default/index.html")
+	articles = Articles.all()
+	return render_template("default/index.html", articles= articles)
 
 @app.route("/about")
 def about():
@@ -23,9 +28,15 @@ def about():
 def contact():
 	return render_template("default/contact.html")
 
+@app.route("/article/<int:article_id>")
+def article_details(article_id):
+	article = Articles.find_one(id=article_id)
+	return render_template("default/article-details.html", article=article)
+
 @app.route("/articles", methods=["GET","POST"])
 def articles():
 	return "listing all articles"
+
 
 # Authentucation routes
 @app.route("/admin")
@@ -59,6 +70,13 @@ def editArticle(article_id):
 	flashMessage = None
 	if request.method == "POST":
 		datas = request.form.to_dict()
+		image = request.files.get('image')
+		if image:
+			filename = secure_filename(image.filename)
+			path = os.path.join(current_app.config['MEDIA_ROOT'], filename)
+			image.save(path)
+			datas['image'] = filename
+
 		Articles.update(datas, ['id'])
 		flashMessage = True
 
